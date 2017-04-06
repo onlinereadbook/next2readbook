58eaa00ec054a201c9bde0ff2264994583e84849
polo13999 <polo13999@gmail.com>
2017-04-06 12:21:20 +0800

    加入sass到webpack 還有使用cronjob撈資料

---
+++ .next/dist/data/lessonData.js
@@ -0,0 +1,8 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var lessonData = exports.lessonData = [{
+    title: "     取貨點  銷售點   推廣點 生產點   生產履歷		", date: "1/ 1"
+}, { title: "2", date: "1/1" }, { title: "3", date: "1/1" }];
